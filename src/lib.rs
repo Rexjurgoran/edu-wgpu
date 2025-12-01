5147a7258197cc9e2c9b3fc993fa963ff451bd86
Marvin Henßler <marvin.henssler@proton.me>
2025-12-02 00:16:26 +0100

    Create HDR loader

--- src/lib.rs
+++ src/lib.rs
@@ -250,15 +250,9 @@ impl State {
 
         let (device, queue) = adapter
             .request_device(&wgpu::DeviceDescriptor {
-                required_features: wgpu::Features::empty(),
-                // WebGL doesn't support all of wgpu's features, so if
-                // we're building for the web, we'll have to disable some.
-                required_limits: if cfg!(target_arch = "wasm32") {
-                    wgpu::Limits::downlevel_webgl2_defaults()
-                } else {
-                    wgpu::Limits::default()
-                },
                 label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
                 memory_hints: Default::default(),
                 trace: wgpu::Trace::Off,
             })
--- src/texture.rs
+++ src/texture.rs
@@ -6,7 +6,7 @@ pub struct Texture {
     pub texture: wgpu::Texture,
     pub view: wgpu::TextureView,
     pub sampler: wgpu::Sampler,
-    size: wgpu::Extent3d,
+    pub size: wgpu::Extent3d,
 }
 
 impl Texture {
